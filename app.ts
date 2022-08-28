4141b0277aa3a86d33224e71ba447aae68ff4306
Javvvve <chenjunwei@192.168.1.9>
2022-08-28 23:48:40 +0800

    feat(User): 删除不必要的interface声明

--- app.ts
+++ app.ts
@@ -2,18 +2,6 @@ import { Application } from "https://deno.land/x/oak/mod.ts";
 import { Logger } from "./middlewares/logger.ts";
 import { Routes } from './api/index.ts';
 
-interface nextFn<> {
-	(arg?: string): void;
-}
-
-interface ContextResp {
-    body: string | unknown
-}
-
-interface Context {
-    response: ContextResp
-}
-
 const app = new Application();
 
 // 使用引入的Middleware代替 any
